7b3b9800bcc9a969a52857e16b746cb0fdd8d141
Aqeel Ghani <aqeel70106@gmail.com>
2026-09-23 21:04:05 +0700

    feat : penambahan fungsi pengukur frekuensi

---
+++ src/7.Pengukur_Frekuensi.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#define input1 25
+
+int counter = 0;
+
+void count_interup();
+
+void setup(){
+    pinMode(input1, INPUT);
+    Serial.begin(115200);
+    Serial.println("ESP32 Started");
+    attachInterrupt(digitalPinToInterrupt(input1), count_interup, RISING);
+}
+
+void loop(){
+    delay(1000);
+
+    Serial.print("Frekuensi ");
+    Serial.print(counter);
+    Serial.println("Hz");
+
+    counter = 0;
+}
+
+void count_interup(){
+    counter++;
+}
